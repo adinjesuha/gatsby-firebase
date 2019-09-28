659dd4641bb761163dee9220a5533803234f0c36
Adin Jesuha <adinjesuha@gmail.com>
2019-09-27 20:51:23 -0600

    edit firebase config file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase"
-import "firebase/firestore"
+import firebase from "firebase/app"
 import "firebase/auth"
+import "firebase/firestore"
 
 const config = {
   apiKey: process.env.GATSBY_FIREBASE_APIKEY,
